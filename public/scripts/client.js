115602c1cc04c5e1a534cf75945e22cde593ac02
Millie8969 <93591040+Millie8969@users.noreply.github.com>
2026-06-21 23:30:26 +0200

    #7 View Transition implementation
    Eerste implementatie voor een MPA-transition.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,3 +2,19 @@
 document.querySelectorAll('.topicLink').forEach(el => {
   el.title = el.textContent;
 });
+
+// View Transition tussen 'Community Dashboard' en 'gebruikers'
+if (document.startViewTransition) {
+      document.addEventListener('click', (e) => {
+        e.preventDefault();
+        const link = e.target.closest('a[href]');
+        if (!link) return;
+
+        const url = new URL(link.href);
+        if (url.origin !== location.origin || url.hash) return;
+
+        document.startViewTransition(() => {
+        window.location.href = link.href;
+      });
+    });
+  }
