77213c13734a79a4a81cf56cd49ab5df26238d95
Millie8969 <93591040+Millie8969@users.noreply.github.com>
2026-06-21 17:10:37 +0200

    Functional laag #4
    Gebruik gemaakt van server-side rendering voor het vullen van de component met dynamische content.

--- server.js
+++ server.js
@@ -5,27 +5,135 @@ import express from 'express'
 // Importeer de Liquid package (ook als dependency via npm geïnstalleerd)
 import { Liquid } from 'liquidjs';
 
+// Importeer de feedsmith package
+import { parseFeed } from 'feedsmith';
+
+// Importeer de JSDOM package
+import { JSDOM } from 'jsdom';
+
 // Maak een nieuwe Express applicatie aan, waarin we de server configureren
-const app = express()
+const app = express();
 
 // Maak werken met data uit formulieren iets prettiger
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true}));
 
 // Gebruik de map 'public' voor statische bestanden (resources zoals CSS, JavaScript, afbeeldingen en fonts)
 // Bestanden in deze map kunnen dus door de browser gebruikt worden
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 // Stel Liquid in als 'view engine'
-const engine = new Liquid()
-app.engine('liquid', engine.express())
+const engine = new Liquid();
+app.engine('liquid', engine.express());
 
 // Stel de map met Liquid templates in
 // Let op: de browser kan deze bestanden niet rechtstreeks laden (zoals voorheen met HTML bestanden)
-app.set('views', './views')
+app.set('views', './views');
 
+const scrapeAndUpdateTweakers = async function() {
+  const tweakersActiveTopicsResponse = await fetch('https://gathering.tweakers.net/rss/list_activetopics')
+  const tweakersActiveTopicsResponseXML = await tweakersActiveTopicsResponse.text()
+  const { feed: tweakersActiveTopicsFeed } = await parseFeed(tweakersActiveTopicsResponseXML)
+  const tweakersLastPoster = tweakersActiveTopicsFeed.items[0].description.substring(13 + tweakersActiveTopicsFeed.items[0].description.indexOf('Last poster: '), tweakersActiveTopicsFeed.items[0].description.indexOf(' at '))
+  const directusUserResponse = await fetch('https://fdnd-agency.directus.app/items/tweakers_users?' + new URLSearchParams({'filter[username]' : tweakersLastPoster}))
+  const directusUserResponseJSON = await directusUserResponse.json()
+  const tweakersLastPosterProfileResponse = await fetch('https://tweakers.net/gallery/' + tweakersLastPoster)
+  const tweakersLastPosterProfileResponseHTML = await tweakersLastPosterProfileResponse.text()
+  const { document: tweakersLastPosterProfileResponseDOM } = (new JSDOM(tweakersLastPosterProfileResponseHTML)).window
+  const tweakersLastPosterProfileLink = tweakersLastPosterProfileResponseDOM.querySelector('a[href^="https://gathering.tweakers.net/forum/find/poster/"]')
+  const tweakersLastPosterPostCount = tweakersLastPosterProfileLink.textContent.replace(/\./g, '')
+  if (directusUserResponseJSON.data.length == 1) {
+    await fetch('https://fdnd-agency.directus.app/items/tweakers_users', {
+      method: 'PATCH',
+      body: JSON.stringify({
+        number_of_posts: tweakersLastPosterPostCount
+      }),
+      headers: {
+        'Content-Type': 'application/json;charset=UTF-8'
+      }
+    })
+  } else {
+    const tweakersLastPosterProfileRegistered = tweakersLastPosterProfileResponseDOM.querySelector('.registered').textContent
+    const tweakersLastPosterProfileRegisteredDateParts = tweakersLastPosterProfileRegistered.substring(18, tweakersLastPosterProfileRegistered.indexOf(', laatste')).split(' ')
+    const months = {januari: '01', februari: '02', maart: '03', april: '04', mei: '05', juni: '06', juli: '07', augustus: '08', september: '09', oktober: 10, november: 11, december: 12}
+    const tweakersLastPosterProfileRegisteredDate = tweakersLastPosterProfileRegisteredDateParts[2] + '-' + months[tweakersLastPosterProfileRegisteredDateParts[1]] + '-' + tweakersLastPosterProfileRegisteredDateParts[0].padStart(2, '0')
+    await fetch('https://fdnd-agency.directus.app/items/tweakers_users', {
+      method: 'POST',
+      body: JSON.stringify({
+        member_since: tweakersLastPosterProfileRegisteredDate,
+        username: tweakersLastPoster,
+        forum_id: tweakersLastPosterProfileLink.getAttribute('href').substring(13 + tweakersLastPosterProfileLink.getAttribute('href').indexOf('/find/poster/')),
+        number_of_posts: tweakersLastPosterPostCount
+      }),
+      headers: {
+        'Content-Type': 'application/json;charset=UTF-8'
+      }
+    })
+  }
+}
 
-console.log('Let op: Er zijn endpoints! Hier heb ik heel hard aan gewerkt.')
+scrapeAndUpdateTweakers()
 
+app.get('/', async function (request, response) {
+  const tweakersRssResponse = await fetch('https://gathering.tweakers.net/rss/')
+  const tweakersRssResponseXML = await tweakersRssResponse.text()
+  const { feed: tweakersRssResponseFeed } = parseFeed(tweakersRssResponseXML)
+  console.log(tweakersRssResponseFeed)
+
+  const tweakersActiveTopicsResponse = await fetch('https://gathering.tweakers.net/rss/list_activetopics')
+  const tweakersActiveTopicsResponseXML = await tweakersActiveTopicsResponse.text()
+  const { feed: tweakersActiveTopicsFeed } = await parseFeed(tweakersActiveTopicsResponseXML)
+
+  const rssItems = []
+  for (const item of tweakersRssResponseFeed.items) {
+    rssItems.push({
+      title: item.title,
+      link: item.link,
+      replies: Number(item.description.substring(9, item.description.indexOf('\n')))
+    })
+  }
+
+  rssItems.sort(function(a, b) {
+   if (a.replies < b.replies) {
+    return 1;
+   } else if (a.replies > b.replies) {
+    return -1;
+   }
+   return 0;
+  })
+
+  function extractDateTime(text) {
+  const match = text.match(/(\d{2})-(\d{2})-(\d{4}) (\d{2}):(\d{2})/);
+  if (!match) return null;
+
+  const [, day, month, year, hours, minutes] = match;
+  return new Date(`${year}-${month}-${day}T${hours}:${minutes}:00`);
+}
+
+  const activeTopicsItems = []
+  for (const item of tweakersActiveTopicsFeed.items) {
+    activeTopicsItems.push({
+      title: item.title,
+      link: item.link,
+      date: extractDateTime(item.description)
+    })
+  }
+
+  activeTopicsItems.sort(function(a, b) {
+   if (a.date < b.date) {
+    return 1;
+   } else if (a.date > b.date) {
+    return -1;
+   }
+   return 0;
+  })
+
+  console.log(tweakersActiveTopicsFeed)
+  
+  response.render('dashboard.liquid', {
+    rss: rssItems,
+    activeTopics: activeTopicsItems
+  })
+})
 
 // GET-endpoint die alle gebruikersdata ophaalt
 // Zie https://expressjs.com/en/5x/api.html#app.get.method over app.get()
