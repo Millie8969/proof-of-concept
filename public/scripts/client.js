56256b1a17f0ec819ffbc772bccac2419ffebab7
Millie8969 <93591040+Millie8969@users.noreply.github.com>
2026-08-23 21:49:32 +0200

    Update client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,4 +1,4 @@
-// Zorgt ervoor dat alle topics worden voorzien van een title (native browser tooltip)
+// Zorgt ervoor dat alle topics worden voorzien van een title via native browser tooltip
 document.querySelectorAll('.topicLink').forEach(el => {
   el.title = el.textContent;
 });
